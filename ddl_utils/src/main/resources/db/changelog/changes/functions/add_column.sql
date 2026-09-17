e414a3dd4a0e5e18015a5af5835cd4653e4ee30a
Lee Horner <eyupmiduck2005@gmail.com>
2026-09-17 12:47:09 -0700

    Delegate add_column to add_columns
    Replace add_column's fragment-building body with a single named-parameter
    call to add_columns using one-element arrays, removing the duplication.
    add_column now also inherits add_columns' blank-default check; cover it in
    AddColumnTest.

--- ddl_utils/src/main/resources/db/changelog/changes/functions/add_column.sql
+++ ddl_utils/src/main/resources/db/changelog/changes/functions/add_column.sql
@@ -14,27 +14,14 @@ CREATE OR REPLACE FUNCTION ddl_utils.add_column(
     SECURITY INVOKER
 AS
 $$
-DECLARE
-    l_fragment text;
 BEGIN
-    l_fragment := pg_catalog.format('ADD COLUMN %I %s', i_column_name, i_column_type);
-
-    -- i_default_value is raw SQL so that expressions such as now() work; pass
-    -- quoted literals (for example '''x''') when a literal is wanted. A NULL
-    -- value means no DEFAULT clause at all. alter_table rejects fragments that
-    -- contain a statement separator or comment.
-    IF i_default_value IS NOT NULL THEN
-        l_fragment := l_fragment || pg_catalog.format(' DEFAULT %s', i_default_value);
-    END IF;
-
-    IF NOT i_nullable THEN
-        l_fragment := l_fragment || ' NOT NULL';
-    END IF;
-
-    PERFORM ddl_utils.alter_table(
+    PERFORM ddl_utils.add_columns(
         i_schema_name => i_schema_name,
         i_table_name => i_table_name,
-        i_alter_table_fragment => l_fragment,
+        i_column_names => ARRAY[i_column_name],
+        i_column_types => ARRAY[i_column_type],
+        i_default_values => ARRAY[i_default_value],
+        i_nullable => ARRAY[i_nullable],
         i_ddl_lock_timeout => i_ddl_lock_timeout,
         i_sleep_time => i_sleep_time,
         i_statement_duration => i_statement_duration
